1e72a8d51a6a7ae6eb56a9bd832571ebd50984c4
warqwert1212 <warqwert12@gmail.com>
2026-04-01 14:41:24 -0500

    Add kernel_entry.asm file

---
+++ kernel_entry.asm
@@ -0,0 +1,17 @@
+[BITS 32]
+MULTIBOOT_HEADER_MAGIC equ 0x1BADB002
+MULTIBOOT_HEADER_FLAGS equ 0x00000001
+MULTIBOOT_CHECKSUM equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+
+section .multiboot
+    align 4
+    dd MULTIBOOT_HEADER_MAGIC
+    dd MULTIBOOT_HEADER_FLAGS
+    dd MULTIBOOT_CHECKSUM
+
+section .text
+global start
+start:
+    cli
+    hlt
+    jmp start
